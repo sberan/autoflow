163d4ad458e075121ac44846714f89d2452493e2
Sam Beran <sberan@gmail.com>
2023-08-09 20:07:30 -0500

    loading states

--- TODO.md
+++ TODO.md
@@ -8,7 +8,7 @@ MVP (stop touching your face)
  - [x] customize model
  - [x] notification as separate "step"
  - [x] make it look decent
- - [ ] camera / model / llm loading states
+ - [x] camera / model / llm loading states
  - [ ] make sure everything works out of the box (incognito)
  - [ ] publish / readme
 
--- web/Camera.tsx
+++ web/Camera.tsx
@@ -3,16 +3,16 @@ import { useLocalStorage } from 'react-use';
 import { VITE_ROBOFLOW_PUBLISHABLE_KEY } from './Main';
 
 export type Predictions = {
-    class: string,
-    bbox: {
-      x: number,
-      y: number,
-      width: number,
-      height: number
-    },
-    confidence: number,
-    color: string
-  }[]
+  class: string,
+  bbox: {
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  },
+  confidence: number,
+  color: string
+}[]
   
 let startCount = 0
 
@@ -21,11 +21,14 @@ export function Camera(props: { onPredictions: (predictions: Predictions) => voi
     const [modelName, setModelName] = useLocalStorage('modelName', 'egohands-public')
     const [modelVersion, setModelVersion] = useLocalStorage('modelVersion', 9)
     const [model, setModel] = useState<any>()
+    const [cameraLoading, setCameraLoading] = useState(true)
+    const [modelLoading, setModelLoading] = useState(true)
     
     useEffect(() => {
       navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
         const video = videoRef.current
         if (video) {
+          setCameraLoading(false)
           video.srcObject = stream
           video.play()
         }
@@ -38,6 +41,7 @@ export function Camera(props: { onPredictions: (predictions: Predictions) => voi
     }, [])
     
     useEffect(() => {
+      setModelLoading(true)
       const roboflow = (window as any).roboflow
       roboflow.auth({
           publishable_key: VITE_ROBOFLOW_PUBLISHABLE_KEY
@@ -46,15 +50,14 @@ export function Camera(props: { onPredictions: (predictions: Predictions) => voi
           version: modelVersion
       }).then(async (model: any) =>{
         setModel(model)
-      })
-      .catch((error: any) => {
+      }).catch((error: any) => {
         console.log(error)
+      }).finally(() => {
+        setModelLoading(false)
       })
-  
     }, [modelName, modelVersion]);
   
     useEffect(() => {
-      let stopped = false
       let id = ++startCount
       const video = videoRef.current!
 
@@ -76,13 +79,13 @@ export function Camera(props: { onPredictions: (predictions: Predictions) => voi
   
       return () => {
         console.log('stopping', id)
-        stopped = true
       }
     }, [model, videoRef, props.onPredictions])
     
     return <>
       <div className="p-6 max-w-lg mx-auto bg-white rounded-xl shadow-md flex flex-col space-y-4 items-center justify-center">
         <label className="text-lg font-semibold text-gray-600">Camera</label>
+        {cameraLoading && <div className="text-blue-500 w-full text-center">Initializing...</div>}
         <div>
           <video ref={videoRef} autoPlay playsInline className="w-full h-40 rounded-md"/>
         </div>
@@ -92,6 +95,7 @@ export function Camera(props: { onPredictions: (predictions: Predictions) => voi
 
       <div className="p-6 max-w-lg mx-auto bg-white rounded-xl shadow-md flex flex-col space-y-4 items-center">
         <label className="text-lg font-semibold text-gray-600">Model</label>
+        {modelLoading && <div className="text-blue-500 w-full text-center">Initializing...</div>}
         <div className="flex items-center space-x-4 w-full">
           <input type="text" placeholder="Model Name" value={modelName} onChange={e => setModelName(e.target.value)} className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 flex-grow"/>
           <input type="text" placeholder="Model Version" value={modelVersion} onChange={e => setModelVersion(+e.target.value)} className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 w-16"/>
